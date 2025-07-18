c50d532f1f3bdb020307e9a20e7c5668d17bbaf5
nabidam <navidabbaspoor@gmail.com>
2025-07-18 20:53:00 +0330

    types

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+type PageDataType = {
+	username: string;
+	title: string;
+	subtitle: string;
+	statement: string;
+	artwork_org: string;
+	artwork_new: string;
+	video: string;
+	sound: string;
+};
