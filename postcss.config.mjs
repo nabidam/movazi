75733d293b20a893e325724a9d4ad851e53511e8
nabidam <navidabbaspoor@gmail.com>
2025-07-20 16:30:33 +0330

    postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,38 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {
+      safelist: [
+        'via-red-950',
+        'via-blue-950',
+        'from-red-900/20',
+        'from-blue-900/20',
+        'from-red-600/20',
+        'from-blue-600/20',
+        'to-red-500/20',
+        'to-blue-500/20',
+        'via-red-100',
+        'via-blue-100',
+        'to-red-200',
+        'to-blue-200',
+        'text-red-400',
+        'text-blue-400',
+        'text-red-900',
+        'text-blue-900',
+        'bg-red-600/10',
+        'bg-blue-600/10',
+        'bg-red-600/15',
+        'bg-blue-600/15',
+        'bg-red-600',
+        'bg-blue-600',
+        'bg-red-500/15',
+        'bg-blue-500/15',
+        'bg-red-500/20',
+        'bg-blue-500/20',
+        'bg-red-400/20',
+        'bg-blue-400/20',
+      ]
+    }
+  },
 };
 
 export default config;
